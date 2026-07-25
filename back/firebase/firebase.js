edc0f42c105c550b9be92ec46a203eb3a4f3ef8d
Bpoojitha <bpoojitha.cs24@bmsce.ac.in>
2026-07-25 20:23:02 +0530

    Use Firebase environment variable

--- back/firebase/firebase.js
+++ back/firebase/firebase.js
@@ -1,10 +1,7 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { readFileSync } from "fs";
 
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("./Key1.json", import.meta.url))
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 initializeApp({
   credential: cert(serviceAccount),
